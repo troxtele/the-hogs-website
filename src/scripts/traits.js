eee9798d6fe869d5d4ec202d05a2affc8b3aada0
Al Riad <indomisba@gmail.com>
2025-03-23 05:30:19 +0600

    update

--- src/scripts/traits.js
+++ src/scripts/traits.js
@@ -15,12 +15,12 @@ const swiper = new Swiper(".swiper", {
     renderBullet: function (index, className) {
       // Array of image URLs for pagination (replace with your own image URLs)
       const images = [
-        "./src/assets/images/traits/hog-1.png",
-        "./src/assets/images/traits/hog-2.png",
-        "./src/assets/images/traits/hog-3.png",
-        "./src/assets/images/traits/hog-4.png",
-        "./src/assets/images/traits/hog-5.png",
-        "./src/assets/images/traits/hog-6.png",
+        "./src/assets/images/traits/hog-1.webp",
+        "./src/assets/images/traits/hog-2.webp",
+        "./src/assets/images/traits/hog-3.webp",
+        "./src/assets/images/traits/hog-4.webp",
+        "./src/assets/images/traits/hog-5.webp",
+        "./src/assets/images/traits/hog-6.webp",
       ];
       return `<div class="${className}">
       <img src="${images[index]}" alt="hog-${index + 1}">
